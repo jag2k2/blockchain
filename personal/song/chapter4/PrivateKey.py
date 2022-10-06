dd7fb316e86c4c02188d6a9ec79c6dfef0aa2c8d
Jeff Tipps <jeff.tipps@ni.com>
2022-10-05 21:01:22 -0500

    ex 5,6

--- personal/song/chapter4/PrivateKey.py
+++ personal/song/chapter4/PrivateKey.py
@@ -1,6 +1,7 @@
 from random import randint
-from S256Point import G, N
+from S256Point import B, G, N
 from Signature import Signature
+from Utility import encode_base58_checksum
 
 import hashlib
 import hmac
@@ -41,3 +42,15 @@ class PrivateKey:
                 return candidate  # <2>
             k = hmac.new(k, v + b'\x00', s256).digest()
             v = hmac.new(k, v, s256).digest()
+
+    def wif(self, compressed=True, testnet=False):
+        secret_bytes = self.secret.to_bytes(32, 'big')
+        if testnet:
+            prefix = b'\xef'
+        else:
+            prefix = b'\x80'
+        if compressed:
+            suffix = b'\x01'
+        else:
+            suffix = b''
+        return encode_base58_checksum(prefix + secret_bytes + suffix)
--- personal/song/chapter4/exercise5.py
+++ personal/song/chapter4/exercise5.py
@@ -5,5 +5,3 @@ if __name__ == '__main__':
     print(PrivateKey(5002).public.address(False, True))
     print(PrivateKey(2020**5).public.address(True, True))
     print(PrivateKey(0x12345deadbeef).public.address(True, False))
-
-    
