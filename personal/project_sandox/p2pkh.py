d90d6632d7f3cfa8f770d41a186a5e44c8f2ff3f
Jeff Tipps <jeff.tipps@ni.com>
2022-11-20 19:13:45 -0600

    added first p2sh txs

---
+++ personal/project_sandox/p2pkh.py
@@ -0,0 +1,23 @@
+from lib.rpc import RpcSocket
+from shared.Tx import Tx
+
+if __name__ == '__main__':
+    from_rpc = RpcSocket({'wallet': 'tn_wallet'})
+    to_rpc = RpcSocket({'wallet':'bob_wallet'})
+
+    all_txins = from_rpc.get_all_utxos()
+    all_amount = from_rpc.get_total_unspent_sats()
+    fee = 500
+    send_amount = int(all_amount * 0.8)
+    change_amount = all_amount - send_amount - fee
+    #address = 'mquczSS7Dz82TeG4Z1sDp23TEi4jR6ApTg'
+    address = None
+    tx_out = to_rpc.get_txout(send_amount, address)
+    tx_out_change = from_rpc.get_txout(change_amount)
+
+    transaction = Tx(1, all_txins, [tx_out, tx_out_change], 0, True)  
+    print(transaction)
+    transaction.sign(from_rpc)
+    print(transaction)
+
+    # tx_id = from_rpc.send_transaction(transaction)
